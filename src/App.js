6323e2b28bbb8f1ea6706499c2831fc03d4c48f7
Alireza <arbor0711@gmail.com>
2023-05-14 11:06:10 +0200

    Form component -set the initial style/input, lable,button,... elements -relate each lable and input

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import { validateEmail } from "./utils";
+
+const PasswordErrorMessage = () => {
+  return (
+    <p className="FieldError">Password should have at least 8 characters</p>
+  );
+};
 
 function App() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState({
+    value: "",
+    isTouched: false,
+  });
+  const [role, setRole] = useState("role");
+
+  const getIsFormValid = () => {
+    // Implement this function
+    return true;
+  };
+
+  const clearForm = () => {
+    // Implement this function
+  };
+
+  const handleSubmit = () => {
+    alert("Account created!");
+    clearForm();
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <fieldset>
+          <h2>Sign Up</h2>
+          <div className="Field">
+            <label htmlFor="firstName">
+              First name <sup>*</sup>
+            </label>
+            <input placeholder="First name" id="firstName" autoFocus />
+          </div>
+          <div className="Field">
+            <label htmlFor="lastName">Last name</label>
+            <input placeholder="Last name" id="lastName" />
+          </div>
+          <div className="Field">
+            <label htmlFor="email">
+              Email address <sup>*</sup>
+            </label>
+            <input placeholder="Email address" id="email" />
+          </div>
+          <div className="Field">
+            <label htmlFor="password">
+              Password <sup>*</sup>
+            </label>
+            <input placeholder="Password" id="password" />
+          </div>
+          <div className="Field">
+            <label htmlFor="role">
+              Role <sup>*</sup>
+            </label>
+            <select id="role">
+              <option value="role">Role</option>
+              <option value="individual">Individual</option>
+              <option value="business">Business</option>
+            </select>
+          </div>
+          <button type="submit" disabled={!getIsFormValid()}>
+            Create account
+          </button>
+        </fieldset>
+      </form>
     </div>
   );
 }
